ed270b929c9a717f2c25c3ac4a03e995dd8df81f
Ike Mwithiga <mwithigaike@gmail.com>
2025-03-24 15:40:45 +0000

    index.js edits

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mwithiga",
+    address: "Pinnacle Towers, h916",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key]= value;
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    let newEmployees = {...employee};
+    delete newEmployees[key];
+    return newEmployees
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee
+}
